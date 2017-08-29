97a0664491ec93a2600dced3fc891f1fbe5b7f2e
Henrique Bonini de Britto Menezes <henrique.b.britto@gmail.com>
2017-08-28 23:39:29 -0300

    Adding first openCV 3.3 test (not working)

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        std::cout << "usage: ./razor <Image Path>" << std::endl;
+        return -1;
+    }
+    Mat image;
+    image = imread(argv[1], 1);
+    if(!image.data) {
+        std::cout << "No image data!!!!" << std::endl;
+        return -2;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+
+    cv::waitKey(0);
+    return 0;
+}
